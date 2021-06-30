da44b38ae396920c1db8c08ffe3cff130bccdea7
michael oreilly <michaelheartbound99@hotmail.com>
2021-06-30 17:34:51 -0600

    footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="text-align-center w-100 mt-auto bg-secondary p-4">
+      <address>
+          <a className="mx-2" href="mailto:Michaelheartbound99@hotmail.com" target="_blank" rel="noreferrer">Michaelheartbound99@hotmail.com</a>
+          <a className="mx-2" href="https://github.com/Wampastomp99" target="_blank" rel="noreferrer">Github</a>
+          <a className="mx-2" href="https://www.linkedin.com" target="_blank" rel="noreferrer">LinkedIn</a>
+      </address>
+      
+      
+    </footer>
+  );
+};
+
+export default Footer;
