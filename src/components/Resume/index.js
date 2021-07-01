806e41ce1f4d5fd0c0244b02c01e31fb0b48a943
michael oreilly <michaelheartbound99@hotmail.com>
2021-06-30 19:34:14 -0600

    utils

---
+++ src/components/Resume/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function Resume() {
+    return (
+        <section className="my-5">
+            <h2>Proficiencies</h2>
+            <p>
+                <ul>
+                    <li>
+                        HTML
+                    </li>
+                    <li>
+                        CSS
+                    </li>
+                    <li>
+                        JavaScript
+                    </li>
+                    <li>
+                        Node.js
+                    </li>
+                    <li>
+                        Express.js
+                    </li>
+                    <li>
+                        MySQL
+                    </li>
+                    <li>
+                        MongoDB
+                    </li>
+
+                </ul>
+            </p>
+            <h4><a href='../../assets/resume/resume.txt' download>Download Resume</a></h4>
+        </section>
+    )
+};
+
+export default Resume;
