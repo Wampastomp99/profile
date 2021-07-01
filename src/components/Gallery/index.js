ba669454627c5b6a2ac67fe99b51689f6ea5e16c
michael oreilly <michaelheartbound99@hotmail.com>
2021-06-30 18:03:47 -0600

    header and gallery

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProjectList from '../ProjectList';
+
+function Gallery(props) {
+    
+    return (
+        <section>
+            <ProjectList/>
+        </section>
+    )
+}
+
+export default Gallery;
